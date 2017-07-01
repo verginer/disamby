cb37f121358499c838da6ad97de4d7826c8a58e0
Luca Verginer <luca@verginer.eu>
2017-07-01 10:44:50 +0200

    adds path for autodoct

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@ import os
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath('.'))
+print(sys.path)
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
 project_root = os.path.dirname(cwd)

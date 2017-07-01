3217fbbebd58185788f24da307781ccd5240630c
Luca Verginer <luca@verginer.eu>
2017-07-01 16:13:02 +0200

    Bump version: 0.2.3 → 0.2.4

--- disamby/__init__.py
+++ disamby/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Luca Verginer"""
 __email__ = 'luca@verginer.eu'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from .core import Disamby
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='disamby',
-    version='0.2.3',
+    version='0.2.4',
     description="Python package to carry out entity disambiguation based on "
                 "string matching",
     long_description=readme + '\n\n' + history,
